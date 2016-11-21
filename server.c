9e6bba563083876583afdbf693052593c158633c
ITE03050362 <a97322810@gmail.com>
2016-11-21 17:20:59 +0800

    Lab09
    Lab09

--- server.c
+++ server.c
@@ -1,124 +1,71 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>    
-#include<sys/socket.h>
-#include<arpa/inet.h> 
-#include<unistd.h>    
+#iclude <stdio.h>
+#include <stdlib.h>
 
-int main(int argc , char *argv[])
-{
-	time_t mytime;
-	mytime=time(NULL);
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
 
-	int socket_desc,new_socket,c;
-	int answer[8];
-	int count_a,count_b,countA,countB;
+#include "doprocessing.h"
 
-	struct sockaddr_in server,client;
-	char *msg;
-	char reply[8],compare[8],msg2[50];
-
-	
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-
-	if (socket_desc == -1)
-	{
-	     printf("Could not create socket");
-	}
-
-	server.sin_family = AF_INET;
-	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8787 );
-
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
-	     puts("bind failed");
-	     return 1;
-	}
-
-	puts("bind done");
-
-	listen(socket_desc , 3);
-
-	puts("Waiting for incoming connections...");
-	c = sizeof(struct sockaddr_in);  
-  
-    while( (new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
-	    puts("Connection accepted");
-	    countA=0; countB=0;
-	    srand( time(NULL) );
+int main( int argc, char *argv[] ) {
+   int sockfd, newsockfd, portno, clilen;
+   char buffer[256];
+   struct sockaddr_in serv_addr, cli_addr;
+   int n, pid;
+   
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   
+   if (sockfd < 0) {
+      perror("ERROR opening socket");
+      exit(1);
+   }
+   
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   portno = 8787;
+   
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_addr.s_addr = INADDR_ANY;
+   serv_addr.sin_port = htons(portno);
+   
  
-		for(count_a=0;count_a<4;count_a++)
-		{
-  		   answer[count_a]=rand()%10; 
+   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+      perror("ERROR on binding");
+      exit(1);
+   }
+   
+ 
+   printf("Welcome to this server, my process id is %d\n", getpid());
 
-  		   for(count_b=0;count_b<count_a;count_b++) 
-  		   { 
-     			if(answer[count_a]==answer[count_b]) 
-     			{	
-     				count_a--; 
-     			} 
-  		   }
-		} 
-         
-
-	     printf("Answer:%d%d%d%d\n",answer[0],answer[1],answer[2],answer[3]);
-
-
-        //傳回client
-
-	while(1)
-	{
-		bzero(reply,8);
-		read(new_socket,reply,8);
-		compare[0]=(reply[0]-'0');
-		compare[1]=(reply[1]-'0');
-		compare[2]=(reply[2]-'0');
-		compare[3]=(reply[3]-'0');
-	
-		for(count_a=0;count_a<4;count_a++)
-		{
-		   if(answer[count_a]==compare[count_a]) 
-		   {
-			countA+=1;
-		   }
-		}
-	
-		for(count_b=0;count_b<=3;count_b++)
-		{
-		   for(count_a=0;count_a<=3;count_a++)
-		   {
-			if(compare[count_a]==answer[count_b] && count_a!=count_b)
-			{
-			     countB++;
-			}
-		   }
-		}
-
-		if(countA==4)
-		{
-			msg="Congratulations!!";
-			write(new_socket , msg, strlen(msg)+1);
-			return 1;
-		}
-
-		else
-		{
-			sprintf(msg2,"%dA%dB",countA,countB);
-			write(new_socket , msg2, strlen(msg2)+1);
-			countA=0;countB=0;
-		}
-
-	        if (new_socket<0)
-	        {
-		      perror("accept failed");
-		      return 1;
-	        }
-		      return 0;
-	}
-	
-    }
-	
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+   
+   while (1) {
+      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,(socklen_t *)  &clilen);
+		
+      if (newsockfd < 0) {
+         perror("ERROR on accept");
+         exit(1);
+      }
+        
+      pid = fork();
+		
+      if (pid < 0) {
+         perror("ERROR on fork");
+         exit(1);
+      }
+      
+      if (pid == 0) {
+        
+         close(sockfd);
+         printf("New connection, my process id is %d\n", getpid());
+         doprocessing(newsockfd);
+         exit(0);
+      }
+	   
+      else {
+         close(newsockfd);
+      }
+		
+   }
 }
-
