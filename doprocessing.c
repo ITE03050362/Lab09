0b6926bd071e8fedf79c51c0b86ce5f727318854
ITE03050362 <a97322810@gmail.com>
2016-11-21 17:16:26 +0800

    Lab09
    Lab09

--- doprocessing.c
+++ doprocessing.c
@@ -11,40 +11,36 @@ void doprocessing (int sock)
    int n;
    char buffer[256];
 
-   //
+   
 	time_t mytime;
 	mytime=time(NULL);  
-	int answer[8];//猜數答案和使用者的回答
-	int count1,count2,countA,countB;//記數用變數
+	int answer[8];
+	int count1,count2,countA,countB;
 	char *message;
 	char reply[8],compare[8],message2[50];
-   //
+   
    if (n < 0) {
       perror("ERROR reading from socket");
       exit(1);
    }
    
- //original
- //  printf("Here is the message: %s\n",buffer);
- //  n = write(sock,"I got your message",18);
 
- //////////////////////
 	countA=0; 
 	countB=0;
 	srand( time(NULL) ); 
 	for(count1=0;count1<4;count1++)
 	{
-		answer[count1]=rand()%10; //亂數產生1~10任意數字並放入陣列內
+		answer[count1]=rand()%10;
 		for(count2=0;count2<count1;count2++) 
 		{ 
-			if(answer[count1]==answer[count2]) //比較是否重複
+			if(answer[count1]==answer[count2])
 			{
-			count1--; //重複則重新產生
+			count1--; 
 			} 
 		}
 	} 
 	printf("Answer:%d%d%d%d\n",answer[0],answer[1],answer[2],answer[3]);
-//Reply to the client
+
 	while(1)
 	{
 		bzero(buffer,256);
@@ -56,7 +52,7 @@ void doprocessing (int sock)
 	
 		for(count1=0;count1<4;count1++)
 		{
-			if(answer[count1]==compare[count1]) //比較數字及位置相同的數
+			if(answer[count1]==compare[count1])
 			{
 				countA+=1;
 			}
@@ -85,7 +81,7 @@ void doprocessing (int sock)
 			countA=0;countB=0;
 		}
 	}
-   //////////////////////
+   
    if (n < 0)
    {
       perror("ERROR writing to socket");
